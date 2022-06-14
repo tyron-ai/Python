3dad5c6c73f0e63a226f4d9e1f3183f188a06c43
Tyron Nyambe(NYMTYR001) <nymtyr001@myuct.ac.za>
2022-06-14 10:41:02 +0200

    Program to convert an amount of minutes to an equivalent amount of days hours and minutes

---
+++ time.py
@@ -0,0 +1,16 @@
+# Program to convert an amount of minutes into an equivalent amount 
+# of days, hours and minutes.
+
+input_str = input("Enter a quantity of minutes: ")
+minutes = int(input_str)
+
+hours = minutes//60
+minutes = minutes%60
+
+days = hours//24
+hours = hours%24
+
+print("The number of days is", days, end=', ')
+print("the number hours is", hours, end=', ')
+print("and the number of minutes is", minutes, end='')
+print(".")
